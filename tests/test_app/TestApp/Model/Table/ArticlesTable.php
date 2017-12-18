6682e29a2e16410a8574bbb59fdb628a24d8c988
Mark Story <markstory@freshbooks.com>
2017-12-18 11:24:57 -0500

    Fix failing tests.

--- tests/test_app/TestApp/Model/Table/ArticlesTable.php
+++ tests/test_app/TestApp/Model/Table/ArticlesTable.php
@@ -2,15 +2,16 @@
 namespace TestApp\Model\Table;
 
 use Authorization\AuthorizationAwareInterface;
-use Cake\Authorization\BouncerTrait;
+use Authorization\BouncerTrait;
 use Cake\ORM\Query;
 use Cake\ORM\Table;
 
-class ArticlesTable extends Table implements AuthorizationAwareInterface {
-
+class ArticlesTable extends Table implements AuthorizationAwareInterface
+{
     use BouncerTrait;
 
-    public function findFieldsByPermission(Query $query) {
+    public function findFieldsByPermission(Query $query)
+    {
         if (!$this->can('selectSpecialFields')) {
             return $query->select([
                 'field1',
