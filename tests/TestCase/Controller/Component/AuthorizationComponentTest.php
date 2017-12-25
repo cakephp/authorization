de4a977dd0fbd9be36d726ac2873b8c9cb31223c
Mark Story <mark@mark-story.com>
2017-12-24 22:50:39 -0500

    Add first draft of authorization component.

---
+++ tests/TestCase/Controller/Component/AuthorizationComponentTest.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         1.0.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Authorization\Test\TestCase\Controller\Component;
+
+use Authorization\AuthorizationService;
+use Authorization\Controller\Component\AuthorizationComponent;
+use Authorization\IdentityDecorator;
+use Authorization\Policy\Exception\MissingMethodException;
+use Authorization\Policy\Exception\MissingPolicyException;
+use Authorization\Policy\OrmResolver;
+use BadMethodCallException;
+use Cake\Controller\ComponentRegistry;
+use Cake\Controller\Controller;
+use Cake\Http\ServerRequest;
+use Cake\Network\Exception\ForbiddenException;
+use Cake\TestSuite\TestCase;
+use InvalidArgumentException;
+use TestApp\Model\Entity\Article;
+
+/**
+ * AuthorizationComponentTest class
+ */
+class AuthorizationComponentTest extends TestCase
+{
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $service = new AuthorizationService(new OrmResolver());
+        $identity = new IdentityDecorator($service, ['id' => 1, 'role' => 'user']);
+
+        $request = new ServerRequest([
+            'params' => ['controller' => 'Articles', 'action' => 'edit'],
+        ]);
+        $request = $request->withAttribute('identity', $identity);
+
+        $this->Controller = new Controller($request);
+        $this->ComponentRegistry = new ComponentRegistry($this->Controller);
+        $this->Auth = new AuthorizationComponent($this->ComponentRegistry);
+    }
+
+    public function testAuthorizeUnresolvedPolicy()
+    {
+        $this->expectException(MissingPolicyException::class);
+
+        $this->Auth->authorize(new \stdClass);
+    }
+
+    public function testAuthorizeFailedCheck()
+    {
+        $this->expectException(ForbiddenException::class);
+
+        $article = new Article(['user_id' => 99]);
+        $this->Auth->authorize($article);
+    }
+
+    public function testAuthorizeSuccessCheckImplictAction()
+    {
+        $article = new Article(['user_id' => 1]);
+        $this->assertNull($this->Auth->authorize($article));
+    }
+
+    public function testAuthorizeSuccessCheckExplictAction()
+    {
+        $article = new Article(['user_id' => 1]);
+        $this->assertNull($this->Auth->authorize($article, 'edit'));
+    }
+
+    public function testAuthorizeBadIdentity()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessageRegexp('/Expected that `identity` would be/');
+
+        $this->Controller->request = $this->Controller->request
+            ->withAttribute('identity', 'derp');
+
+        $article = new Article(['user_id' => 1]);
+        $this->Auth->authorize($article);
+    }
+
+    public function testAuthorizeCustomException()
+    {
+        $this->expectException(BadMethodCallException::class);
+        $this->Auth->config('forbiddenException', BadMethodCallException::class);
+
+        $article = new Article(['user_id' => 99]);
+        $this->Auth->authorize($article);
+    }
+}
