4a68b32eea32db3c3689123e3e7c8d0ac76cf94b
Robert Pustułka <robert.pustulka@gmail.com>
2018-01-26 10:06:58 +0100

    Add CakePHP redirect handler.

---
+++ tests/TestCase/Middleware/Handler/CakeRedirectHandlerTest.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         1.0.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace Authorization\Test\TestCase\Middleware\Handler;
+
+use Authorization\Exception\Exception;
+use Authorization\Middleware\Handler\CakeRedirectHandler;
+use Cake\Http\Response;
+use Cake\Http\ServerRequest;
+use Cake\Routing\Router;
+use Cake\TestSuite\TestCase;
+
+/**
+ * Description of CakeRedirectHandlerTest
+ *
+ * @author Robert Pustułka <robert.pustulka@gmail.com>
+ */
+class CakeRedirectHandlerTest extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        Router::reload();
+        Router::fullBaseUrl('http://localhost');
+        Router::connect(
+            '/login',
+            ['controller' => 'Users', 'action' => 'login'],
+            ['_name' => 'login']
+        );
+        Router::connect('/:controller/:action');
+    }
+
+    public function testHandleRedirectionDefault()
+    {
+        $handler = new CakeRedirectHandler;
+
+        $exception = new Exception;
+        $request = new ServerRequest;
+        $response = new Response;
+
+        $response = $handler->handle($exception, $request, $response, [
+            'exceptions' => [
+                Exception::class,
+            ],
+        ]);
+
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertEquals('/login?url=http%3A%2F%2Flocalhost%2F', $response->getHeaderLine('Location'));
+    }
+
+    public function testHandleRedirectionNamed()
+    {
+        $handler = new CakeRedirectHandler;
+
+        $exception = new Exception;
+        $request = new ServerRequest;
+        $response = new Response;
+
+        $response = $handler->handle($exception, $request, $response, [
+            'exceptions' => [
+                Exception::class,
+            ],
+            'url' => [
+                '_name' => 'login'
+            ]
+        ]);
+
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertEquals('/login?url=http%3A%2F%2Flocalhost%2F', $response->getHeaderLine('Location'));
+    }
+
+    public function testHandleRedirectionWithQuery()
+    {
+        $handler = new CakeRedirectHandler;
+
+        $exception = new Exception;
+        $request = new ServerRequest;
+        $response = new Response;
+
+        $response = $handler->handle($exception, $request, $response, [
+            'exceptions' => [
+                Exception::class,
+            ],
+            'url' => [
+                '_name' => 'login',
+                '?' => [
+                    'foo' => 'bar'
+                ],
+            ]
+        ]);
+
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertEquals('/login?foo=bar&url=http%3A%2F%2Flocalhost%2F', $response->getHeaderLine('Location'));
+    }
+
+    public function testHandleRedirectionNoQuery()
+    {
+        $handler = new CakeRedirectHandler;
+
+        $exception = new Exception;
+        $request = new ServerRequest;
+        $response = new Response;
+
+        $response = $handler->handle($exception, $request, $response, [
+            'exceptions' => [
+                Exception::class,
+            ],
+            'queryParam' => null,
+        ]);
+
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertEquals('/login', $response->getHeaderLine('Location'));
+    }
+}
