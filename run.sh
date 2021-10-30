614e25cac6533ea5f5dc616321e8458f64e5ef8b
Mark Story <mark@mark-story.com>
2021-10-29 23:35:31 -0400

    Improve es host name url.
    Use the variable set by dokku

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # Update elasticsearch indexes.
 for lang in ${LANGS}
 do
-    php /data/populate_search_index.php --source="$SEARCH_SOURCE/$*" --lang="$lang" --host="dokku-elasticsearch-searchv2" --url-prefix="$SEARCH_URL_PREFIX"
+    php /data/populate_search_index.php --source="$SEARCH_SOURCE/$*" --lang="$lang" --host="$ELASTICSEARCH_URL" --url-prefix="$SEARCH_URL_PREFIX"
 done
 
 # Run nginx like normal.
