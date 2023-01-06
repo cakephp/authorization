c6aa5e3699ab7e97498807ce8d76a481d17a9ab2
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2023-01-06 16:08:12 +0100

    add missing param types

--- src/IdentityDecorator.php
+++ src/IdentityDecorator.php
@@ -76,7 +76,7 @@ class IdentityDecorator implements IdentityInterface
     /**
      * @inheritDoc
      */
-    public function applyScope(string $action, $resource, mixed ...$optionalArgs): mixed
+    public function applyScope(string $action, mixed $resource, mixed ...$optionalArgs): mixed
     {
         return $this->authorization->applyScope($this, $action, $resource, ...$optionalArgs);
     }
